5ec3fe903d9d70a9f122d6e6e2797b171f1e3591
JaretWright <jaret.wright@georgiancollege.ca>
2021-03-18 13:21:40 -0400

    Created tests for the Person class

---
+++ src/models/Person.java
@@ -0,0 +1,47 @@
+package models;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String firstName, lastName, address;
+    private LocalDate birthday;
+
+    public Person(String firstName, String lastName, String address, LocalDate birthday) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setAddress(address);
+        setBirthday(birthday);
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+}
